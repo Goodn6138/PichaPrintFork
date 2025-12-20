ae760858ca4b2ed860717c6a609bc8fd6d4ee3de
Good'n Paul <73657550+Goodn6138@users.noreply.github.com>
2025-12-21 01:20:20 +0300

    Refactor simplify_mesh to remove duplicate decimation calls
    Removed redundant calls to meshing_decimation_quadric_edge_collapse_with_texture.

--- scripts/inference_triposg.py
+++ scripts/inference_triposg.py
@@ -67,9 +67,8 @@ def simplify_mesh(mesh: trimesh.Trimesh, n_faces): #issues with pymesh library
     if mesh.faces.shape[0] > n_faces:
         ms = mesh_to_pymesh(mesh.vertices, mesh.faces)
         ms.meshing_merge_close_vertices()
-        ms.meshing_decimation_quadric_edge_collapse_with_texture(targetfacenum = n_faces)
-
-        ms.meshing_decimation_quadric_edge_collapse_with_texture(targetfacenum = n_faces)
+     #   ms.meshing_decimation_quadric_edge_collapse_with_texture(targetfacenum = n_faces)
+      #  ms.meshing_decimation_quadric_edge_collapse_with_texture(targetfacenum = n_faces)
         try:
            ms.meshing_decimation_quadric_edge_collapse(targetfacenum=n_faces)
         except AttributeError:          # PyMeshLab ≥ 2022.12
